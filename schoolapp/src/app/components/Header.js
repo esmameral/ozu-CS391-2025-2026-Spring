f30ea1b2e09d69bad4b52f91ad6e0e564cf5726c
esmameral <esma.meral@gmail.com>
2026-03-27 11:13:38 +0300

    initial reactjs application

---
+++ schoolapp/src/app/components/Header.js
@@ -0,0 +1,7 @@
+import Name from "./Name"
+const Header = () => {
+    return <div> My Web Application
+        <Name />
+    </div>
+}
+export default Header;
---
+++ schoolapp/src/app/components/Name.js
@@ -0,0 +1,9 @@
+const Name = () => {
+    return <div>
+        <ul>
+            <li>Ahmet</li>
+            <li>Mehmet</li>
+        </ul>
+    </div>
+}
+export default Name;
