603ee4d72931f9c7242d5170c6882475564de420
esmameral <esma.meral@gmail.com>
2026-04-08 17:10:51 +0300

    refactoring

--- schoolapp/src/app/components/Catalog.js
+++ schoolapp/src/app/components/Catalog.js
@@ -1,29 +1,36 @@
 
 import Rate from "./Rate";
 
-const Catalog = ({products}) => {
+const Catalog = ({ products }) => {
 
 
     return (
         <table>
-            <tr><th>Name</th><th>Price</th><th>Description</th><th>Operations</th></tr>
-        { products.map( (item, index) => {
-            return (<tr key={index}>
-                        <td>{item.name}</td>
-                        <td>{item.price}</td>
-                        <td>{item.description}</td>
-                        <td><Rate/></td>
-                        </tr>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Description</th>
+                    <th>Operations</th>
+                </tr>
+            </thead>
+            {products.map((item, index) => {
+                return (<tr key={index}>
+                    <td>{item.name}</td>
+                    <td>{item.price}</td>
+                    <td>{item.description}</td>
+                    <td><Rate /></td>
+                </tr>
+
+                )
+            }
 
             )
-        }
-        
-        )
 
 
 
 
-        }
-    </table>)
+            }
+        </table>)
 }
 export default Catalog;
